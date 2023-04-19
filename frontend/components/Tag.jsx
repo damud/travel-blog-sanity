ece489539fd6d8713d5953dd5a477dbfab666bcf
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-04-19 11:29:37 +0200

    Display colored tags in cards

--- frontend/components/Tag.jsx
+++ frontend/components/Tag.jsx
@@ -1,6 +1,20 @@
 const Tag = ({ title }) => {
+  const getColor = () => {
+    let color;
+    switch (title.toLowerCase()) {
+        case "travel":
+            color = rgb(210,138,138);
+            break;
+        case "holiday":
+            color = rgb(169,210,138);
+             break; 
+        default:
+            color = rgb(129,138,210);
+    }
+    return color;
+  }
   return (
-    <div>{ title }</div>
+    <div style={{backgroundColor: getColor()}} className="tag">{ title }</div>
   )
 }
 
