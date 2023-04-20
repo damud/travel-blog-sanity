4d81b3503ebe9ceab5cfd820cc92f35440e88fbe
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-04-20 09:54:25 +0200

    Changed sanity version from v3 to v2

--- backend/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: '8o6d2nic',
-    dataset: 'production'
-  }
-})
--- backend/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import post from "./post";
-import blockContent from "./blockContent";
-import category from "./category";
-import author from "./author";
-
-
-export const schemaTypes = [post, blockContent, category, author]
